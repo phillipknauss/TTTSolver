6e58d99f02d5b02db214319d8f98c3744cd63faf
phillipknauss <philllip.knauss@gmail.com>
2011-10-09 23:43:59 -0700

    Add GUI

---
+++ WinTTT/TTTGui.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WinTTT
+{
+	partial class TTTGui
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TTTGui));
+			this.pic00 = new System.Windows.Forms.PictureBox();
+			this.pic10 = new System.Windows.Forms.PictureBox();
+			this.pic20 = new System.Windows.Forms.PictureBox();
+			this.pic01 = new System.Windows.Forms.PictureBox();
+			this.pic11 = new System.Windows.Forms.PictureBox();
+			this.pic21 = new System.Windows.Forms.PictureBox();
+			this.pic02 = new System.Windows.Forms.PictureBox();
+			this.pic12 = new System.Windows.Forms.PictureBox();
+			this.pic22 = new System.Windows.Forms.PictureBox();
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.mainLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.solverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.takeNextTurnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.serializedText = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pic00)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic10)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic20)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic01)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic21)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic02)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic22)).BeginInit();
+			this.statusStrip.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pic00
+			// 
+			this.pic00.Image = ((System.Drawing.Image)(resources.GetObject("pic00.Image")));
+			this.pic00.Location = new System.Drawing.Point(12, 44);
+			this.pic00.Name = "pic00";
+			this.pic00.Size = new System.Drawing.Size(100, 100);
+			this.pic00.TabIndex = 0;
+			this.pic00.TabStop = false;
+			this.pic00.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic10
+			// 
+			this.pic10.Image = ((System.Drawing.Image)(resources.GetObject("pic10.Image")));
+			this.pic10.Location = new System.Drawing.Point(118, 44);
+			this.pic10.Name = "pic10";
+			this.pic10.Size = new System.Drawing.Size(100, 100);
+			this.pic10.TabIndex = 1;
+			this.pic10.TabStop = false;
+			this.pic10.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic20
+			// 
+			this.pic20.Image = ((System.Drawing.Image)(resources.GetObject("pic20.Image")));
+			this.pic20.Location = new System.Drawing.Point(224, 44);
+			this.pic20.Name = "pic20";
+			this.pic20.Size = new System.Drawing.Size(100, 100);
+			this.pic20.TabIndex = 2;
+			this.pic20.TabStop = false;
+			this.pic20.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic01
+			// 
+			this.pic01.Image = ((System.Drawing.Image)(resources.GetObject("pic01.Image")));
+			this.pic01.Location = new System.Drawing.Point(12, 150);
+			this.pic01.Name = "pic01";
+			this.pic01.Size = new System.Drawing.Size(100, 100);
+			this.pic01.TabIndex = 3;
+			this.pic01.TabStop = false;
+			this.pic01.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic11
+			// 
+			this.pic11.Image = ((System.Drawing.Image)(resources.GetObject("pic11.Image")));
+			this.pic11.Location = new System.Drawing.Point(118, 150);
+			this.pic11.Name = "pic11";
+			this.pic11.Size = new System.Drawing.Size(100, 100);
+			this.pic11.TabIndex = 4;
+			this.pic11.TabStop = false;
+			this.pic11.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic21
+			// 
+			this.pic21.Image = ((System.Drawing.Image)(resources.GetObject("pic21.Image")));
+			this.pic21.Location = new System.Drawing.Point(224, 150);
+			this.pic21.Name = "pic21";
+			this.pic21.Size = new System.Drawing.Size(100, 100);
+			this.pic21.TabIndex = 5;
+			this.pic21.TabStop = false;
+			this.pic21.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic02
+			// 
+			this.pic02.Image = ((System.Drawing.Image)(resources.GetObject("pic02.Image")));
+			this.pic02.Location = new System.Drawing.Point(12, 256);
+			this.pic02.Name = "pic02";
+			this.pic02.Size = new System.Drawing.Size(100, 100);
+			this.pic02.TabIndex = 6;
+			this.pic02.TabStop = false;
+			this.pic02.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic12
+			// 
+			this.pic12.Image = ((System.Drawing.Image)(resources.GetObject("pic12.Image")));
+			this.pic12.Location = new System.Drawing.Point(118, 256);
+			this.pic12.Name = "pic12";
+			this.pic12.Size = new System.Drawing.Size(100, 100);
+			this.pic12.TabIndex = 7;
+			this.pic12.TabStop = false;
+			this.pic12.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// pic22
+			// 
+			this.pic22.Image = ((System.Drawing.Image)(resources.GetObject("pic22.Image")));
+			this.pic22.Location = new System.Drawing.Point(224, 256);
+			this.pic22.Name = "pic22";
+			this.pic22.Size = new System.Drawing.Size(100, 100);
+			this.pic22.TabIndex = 8;
+			this.pic22.TabStop = false;
+			this.pic22.Click += new System.EventHandler(this.pic_Click);
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainLabel});
+			this.statusStrip.Location = new System.Drawing.Point(0, 389);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(336, 25);
+			this.statusStrip.TabIndex = 9;
+			this.statusStrip.Text = "statusStrip1";
+			// 
+			// mainLabel
+			// 
+			this.mainLabel.Name = "mainLabel";
+			this.mainLabel.Size = new System.Drawing.Size(66, 20);
+			this.mainLabel.Text = "Nothing.";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.solverToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(336, 28);
+			this.menuStrip1.TabIndex = 10;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// gameToolStripMenuItem
+			// 
+			this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+			this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+			this.gameToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+			this.gameToolStripMenuItem.Text = "Game";
+			// 
+			// newGameToolStripMenuItem
+			// 
+			this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+			this.newGameToolStripMenuItem.Size = new System.Drawing.Size(151, 24);
+			this.newGameToolStripMenuItem.Text = "New Game";
+			this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+			// 
+			// solverToolStripMenuItem
+			// 
+			this.solverToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.takeNextTurnToolStripMenuItem});
+			this.solverToolStripMenuItem.Name = "solverToolStripMenuItem";
+			this.solverToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+			this.solverToolStripMenuItem.Text = "Solver";
+			// 
+			// takeNextTurnToolStripMenuItem
+			// 
+			this.takeNextTurnToolStripMenuItem.Name = "takeNextTurnToolStripMenuItem";
+			this.takeNextTurnToolStripMenuItem.Size = new System.Drawing.Size(177, 24);
+			this.takeNextTurnToolStripMenuItem.Text = "Take Next Turn";
+			this.takeNextTurnToolStripMenuItem.Click += new System.EventHandler(this.takeNextTurnToolStripMenuItem_Click);
+			// 
+			// serializedText
+			// 
+			this.serializedText.Location = new System.Drawing.Point(330, 44);
+			this.serializedText.Multiline = true;
+			this.serializedText.Name = "serializedText";
+			this.serializedText.Size = new System.Drawing.Size(207, 100);
+			this.serializedText.TabIndex = 11;
+			this.serializedText.Visible = false;
+			// 
+			// TTTGui
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(336, 414);
+			this.Controls.Add(this.serializedText);
+			this.Controls.Add(this.statusStrip);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.pic22);
+			this.Controls.Add(this.pic12);
+			this.Controls.Add(this.pic02);
+			this.Controls.Add(this.pic21);
+			this.Controls.Add(this.pic11);
+			this.Controls.Add(this.pic01);
+			this.Controls.Add(this.pic20);
+			this.Controls.Add(this.pic10);
+			this.Controls.Add(this.pic00);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "TTTGui";
+			this.Text = "Tic Tac Toe v1.0";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pic00)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic10)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic20)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic01)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic21)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic02)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic22)).EndInit();
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pic00;
+		private System.Windows.Forms.PictureBox pic10;
+		private System.Windows.Forms.PictureBox pic20;
+		private System.Windows.Forms.PictureBox pic01;
+		private System.Windows.Forms.PictureBox pic11;
+		private System.Windows.Forms.PictureBox pic21;
+		private System.Windows.Forms.PictureBox pic02;
+		private System.Windows.Forms.PictureBox pic12;
+		private System.Windows.Forms.PictureBox pic22;
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.ToolStripStatusLabel mainLabel;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem solverToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem takeNextTurnToolStripMenuItem;
+		private System.Windows.Forms.TextBox serializedText;
+	}
+}
+
---
+++ WinTTT/TTTGui.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TTTSolver_Lib;
+using TTTSolver_Solver;
+
+namespace WinTTT
+{
+	public partial class TTTGui : Form
+	{
+		TTTBoard Game;
+		TTTSolver Solver;
+		
+		Dictionary<Point,PictureBox> locations;
+
+		public TTTGui()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			locations = new Dictionary<Point,PictureBox>()
+			{
+				{ new Point(0,0), pic00 }, { new Point(0,1), pic01 }, { new Point(0,2), pic02 },
+				{ new Point(1,0), pic10 }, { new Point(1,1), pic11 }, { new Point(1,2), pic12 },
+				{ new Point(2,0), pic20 }, { new Point(2,1), pic21 }, { new Point(2,2), pic22 }
+			};
+			Normalize();
+			NewGame();
+
+			MessageBox.Show("Instructions: You know the rules. Current turn is displayed in the status bar.\nYou can use the menu item Solver > Take Next Turn to have the solver calculate the next turn.\nUse this to make the computer play itself, or play your choice of X or O");
+		}
+
+		void Normalize()
+		{
+			foreach (var l in locations)
+			{
+				l.Value.Tag = "";
+				l.Value.Image = Image.FromFile("empty_label.gif");
+				l.Value.Enabled = true;
+			}
+		}
+		void NewGame()
+		{
+			Game = new TTTBoard();
+			Game.TurnTaken += new TTTBoard.TurnHandler(Game_TurnTaken);
+			Solver = new BruteForceTTTSolver(Game);
+			//Solver = new MiniMaxSolver(Game);
+			mainLabel.Text = Game.CurrentPlayer + "'s turn.";
+		}
+
+		void Game_TurnTaken(TTTBoard board)
+		{
+			UpdateFromGame(board.LastPlay.X, board.LastPlay.Y, Game.LastPlayer);
+		}
+		void Disable()
+		{
+			foreach (var l in locations)
+			{
+				l.Value.Enabled = false;
+			}
+		}
+		void Enable()
+		{
+			foreach (var l in locations)
+			{
+				l.Value.Enabled = true;
+			}
+		}
+
+		public void TakeTurn(int x, int y, PictureBox sender)
+		{
+			string tag = (((PictureBox)sender).Tag == null ? "" : ((PictureBox)sender).Tag.ToString());
+			if (String.Equals(tag, "X") || String.Equals(tag, "O"))
+			{
+				MessageBox.Show("That position has already been taken.");
+				return;
+			}
+			Game.TakeTurn(x, y);
+		}
+
+		public PictureBox BoxFromPlay(int x, int y)
+		{
+			string locStr = x.ToString() + y.ToString();
+			switch (locStr)
+			{
+				case "00":
+					return pic00;
+				case "01":
+					return pic01;
+				case "02":
+					return pic02;
+				case "10":
+					return pic10;
+				case "11":
+					return pic11;
+				case "12":
+					return pic12;
+				case "20":
+					return pic20;
+				case "21":
+					return pic21;
+				case "22":
+					return pic22;
+				default:
+					return null;
+			}
+		}
+
+		public void UpdateBoardImages()
+		{
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 3; y++)
+				{
+					var loc = Game.GetLocation(x, y);
+					switch (loc.State)
+					{
+						case LocationState.X:
+							BoxFromPlay(x, y).Image = Image.FromFile("x.png");
+							break;
+						case LocationState.O:
+							BoxFromPlay(x, y).Image = Image.FromFile("o.gif");
+							break;
+						default:
+							BoxFromPlay(x, y).Image = Image.FromFile("empty_label.gif");
+							break;
+					}
+				}
+			}
+			
+		}
+
+		public void UpdateFromGame(int x, int y, Player actingPlayer)
+		{
+			var sender = BoxFromPlay(x, y);
+			
+			if (sender == null)
+			{
+				sender = new PictureBox();
+			}
+			
+			UpdateBoardImages();
+			((PictureBox)sender).Tag = actingPlayer.ToString();
+			if (Game.GameOver)
+			{
+				string endMessage = "Game Over.";
+				if (Game.XWins)
+				{
+					endMessage += " X Wins!";
+				}
+				if (Game.OWins)
+				{
+					endMessage += " O Wins!";
+				}
+				mainLabel.Text = endMessage;
+				MessageBox.Show(endMessage);
+				Disable();
+				return;
+			}
+
+			mainLabel.Text = Game.CurrentPlayer + "'s turn.";
+			serializedText.Text = Game.SerializeWeights();
+		}
+		PictureBox lastBox = null;
+		Player lastPlayer = Player.X;
+		private void pic_Click(object sender, EventArgs e)
+		{
+			string boxName = ((PictureBox)sender).Name;
+			int x = -1;
+			int y = -1;
+			Int32.TryParse(boxName[boxName.Length - 2].ToString(), out x);
+			Int32.TryParse(boxName[boxName.Length - 1].ToString(), out y);
+			if (x < 0 || y < 0) { return; }
+			lastPlayer = Game.CurrentPlayer;
+			lastBox = (PictureBox)sender;
+			TakeTurn(x, y, lastBox);
+		}
+
+		private void toolStripTextBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		public void NormalizeAndNewGame()
+		{
+			Normalize();
+			NewGame();
+		}
+
+		private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			NormalizeAndNewGame();
+		}
+
+		private void takeNextTurnToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (!Game.GameOver)
+			{
+				Solver.PlayNextMove();
+			}
+		}
+	}
+}
